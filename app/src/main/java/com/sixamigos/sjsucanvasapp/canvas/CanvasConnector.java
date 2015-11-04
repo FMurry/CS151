00ca39e82c2dad41aa87d2bfc0f82eae08bd4637
Alex Heritier <alex.heritier@gmail.com>
2015-11-03 16:22:34 -0800

    Added a package for the canvas code + added the canvas connector class

---
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/CanvasConnector.java
@@ -0,0 +1,12 @@
+package com.sixamigos.sjsucanvasapp.canvas;
+
+/**
+ * Created by Alex on 11/3/15.
+ */
+public class CanvasConnector {
+    private static final String TAG = "canvas.CanvasConnector";
+
+    public CanvasConnector() {
+        System.out.println(TAG + ": yolo");
+    }
+}
