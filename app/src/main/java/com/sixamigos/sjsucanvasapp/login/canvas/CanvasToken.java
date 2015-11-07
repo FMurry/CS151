85afb386980cff4f744534f49f680d969dc8304a
Christopher Bachner <christopherbachner@compy.fastnet>
2015-11-06 19:25:02 -0800

    improved it even more

--- app/src/main/java/com/sixamigos/sjsucanvasapp/login/canvas/CanvasToken.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/login/canvas/CanvasToken.java
@@ -1,20 +1,33 @@
 package com.sixamigos.sjsucanvasapp.login.canvas;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import com.sixamigos.sjsucanvasapp.login.LogInActivity;
+
 /**
  * Created by christopherbachner on 11/6/15.
  */
 public class CanvasToken {
 
-    private String CanvasToken = null;
+    private static String CanvasToken = null;
     private String CanvasServer = null;
+    private Activity logInActivity = null;
 
     /**
      * Creates a CanvasToken object that handles authorization with canvasServer and sets up token so further API calls can be done.
      * @param canvasServer CanvasServer that should be used for the authorization. (e.g. sjsu.instructure.com) (No http etc.)
+     * @param logInActivity A reference to any activity where the OAUTH2 web view should be opened if token does not exist.
      */
-    public CanvasToken(String canvasServer) {
+    public CanvasToken(String canvasServer, Activity logInActivity) {
         CanvasServer = canvasServer;
+        this.logInActivity = logInActivity;
     }
+
+
+    //TODO implement saved encrypted token
 
     /**
      * Used by other classes to get the token. If token does not exist, authorize method will be called.
@@ -44,7 +57,13 @@ public class CanvasToken {
      * @throws CanvasLoginFailureException If for some reason the login fails or user cancels login.
      */
     private void authorize() throws CanvasLoginFailureException {
-        throw new CanvasLoginFailureException("Not implemented yet.");
+        try {
+            logInActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://" + this.CanvasServer)));
+            CanvasToken = "123";
+        } catch (Exception e)
+        {
+
+        }
 
     }
 }
