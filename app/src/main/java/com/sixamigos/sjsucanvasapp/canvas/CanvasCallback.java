23f8c4204e7551e4eb271df9c37b10f2c999d896
Alex Heritier <alex.heritier@gmail.com>
2015-11-12 15:55:12 -0800

    working on solving the authentication errors when connecting to canvas

---
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/CanvasCallback.java
@@ -0,0 +1,10 @@
+package com.sixamigos.sjsucanvasapp.canvas;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Alex on 11/12/15.
+ */
+public interface CanvasCallback {
+    void call(JSONObject data);
+}
