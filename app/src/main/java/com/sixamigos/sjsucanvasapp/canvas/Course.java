4a75647b6d5431d154d79e05aa5301cb2a395407
Alex Heritier <alex.heritier@gmail.com>
2015-11-12 12:56:14 -0800

    Added a course object

---
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/Course.java
@@ -0,0 +1,13 @@
+package com.sixamigos.sjsucanvasapp.canvas;
+
+/**
+ * @author  Alex Heritier
+ */
+public class Course {
+    private String name;
+    private int grade;
+
+    public Course() {
+
+    }
+}
