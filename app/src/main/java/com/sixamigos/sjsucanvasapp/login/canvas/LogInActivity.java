f924c286d7dde718339678f542b86e42d4301df0
Christopher Bachner <christopherbachner@compy.fastnet>
2015-11-12 20:48:07 -0800

    Finished login + polished UI

--- app/src/main/java/com/sixamigos/sjsucanvasapp/login/canvas/LogInActivity.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/login/canvas/LogInActivity.java
@@ -3,9 +3,12 @@ package com.sixamigos.sjsucanvasapp.login.canvas;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,44 +23,93 @@ import org.w3c.dom.Text;
 
 public class LogInActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_log_in);
+    /**
+     * Handles validation of user input.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
 
-    findViewById(R.id.logInErrorMessage).setVisibility(View.INVISIBLE);
+        final EditText logInEditText = (EditText) findViewById(R.id.logInEditText);
+        final Button logInButton = (Button) findViewById(R.id.logInButton);
+        final TextView errorText = (TextView) findViewById(R.id.logInErrorMessage);
 
-    WebView webView = (WebView) findViewById(R.id.logInWebView);
-    webView.setWebViewClient(new WebViewClient());
-    webView.loadUrl("https://sjsu.instructure.com/profile/settings");
-    webView.requestFocus();
+        errorText.setVisibility(View.INVISIBLE);
+        logInButton.setEnabled(false);
+        logInEditText.requestFocus();
+        logInEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-  }
+            }
 
-  /**
-   * Gets called when user hits Log In button. Checks if token has correct size or not. If size is correct, home activity gets launched.
-   * @param view
-   */
-  public void logIn (View view)
-  {
-    EditText logInEditText = (EditText)findViewById(R.id.logInEditText);
-    TextView errorText = (TextView)findViewById(R.id.logInErrorMessage);
-    String errorString = errorText.getText().toString();
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (logInEditText.length() == 67) {
+                    logInButton.setEnabled(true);
+                    errorText.setVisibility(TextView.INVISIBLE);
+                } else {
+                    logInButton.setEnabled(false);
+                    errorText.setVisibility(TextView.VISIBLE);
 
-    if (logInEditText.length() == 0 || logInEditText.length() != 67)
-    {
+                }
+            }
 
-      errorText.setText(errorString + logInEditText.length());
-      errorText.setVisibility(TextView.VISIBLE);
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Used to fix wrong status bar color
+        this.getWindow().setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
 
     }
-    else
-    {
-      CanvasToken.setCanvasToken(logInEditText.getText().toString());
-      Intent i = new Intent(this, HomeActivity.class);
-      startActivity(i);
+
+    /**
+     * Gets called when user hits Log In button. Is only enabled if the input text is exactly 67 characters long. If size is correct, home activity gets launched.
+     *
+     * @param view
+     */
+    public void logIn(View view) {
+        EditText logInEditText = (EditText) findViewById(R.id.logInEditText);
+        TextView errorText = (TextView) findViewById(R.id.logInErrorMessage);
+        String errorString = errorText.getText().toString();
+
+        if (logInEditText.length() == 0 || logInEditText.length() != 67) {
+
+            errorText.setText(errorString + logInEditText.length());
+            errorText.setVisibility(TextView.VISIBLE);
+
+        } else {
+            CanvasToken.setCanvasToken(logInEditText.getText().toString());
+            Intent i = new Intent(this, HomeActivity.class);
+            startActivity(i);
+        }
+
     }
 
-  }
+    /**
+     * Launches the webview for the user to be able to generate the token manually.
+     * @param view
+     */
+    public void requestAccess(View view) {
+        startActivity(new Intent(this, LoginWebViewActivity.class));
+    }
+
+    /**
+     * Allows user to skip setup to only use non-canvas features.
+     * @param view
+     */
+    public void skipSetup(View view) {
+        CanvasToken.setCanvasToken("LOCAL");
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
 
 }
+
+
