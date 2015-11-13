e29506ca4c555a12378b47355893709dae6bd47b
Frederic <frederic.wesley.murry@gmail.com>
2015-11-12 20:02:45 -0800

    Added some methods to course class

--- app/src/main/java/com/sixamigos/sjsucanvasapp/Courses/Course.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/Courses/Course.java
@@ -8,9 +8,13 @@ public class Course {
 
     private int id;
     private String courseName; //Ex: 151
-    private String fullName; //Ex: Object Oriented Programming
     private String roomNumber; //Ex: DH 135
+    private String fullName; //Ex: Object Oriented Programming
 
+    //private List<Assignments> assignments;
+
+
+    // CONTSTRUCTORS_______________________________________
 
     public Course(){
 
@@ -19,6 +23,39 @@ public class Course {
     public Course(String courseName,String roomNumber){
         this.courseName = courseName;
         this.roomNumber = roomNumber;
+    }
+
+    public Course(String courseName,String roomNumber, String fullName){
+        this.courseName = courseName;
+        this.roomNumber = roomNumber;
+        this.fullName = fullName;
+    }
+
+    //Accessors____________________________________________
+    public int getId(){
+        return id;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public String getRoomNumber(){
+        return roomNumber;
+    }
+
+
+    //Mutators_______________________________________________
+    public void setCourseName(String newCourseName){
+        courseName = newCourseName;
+    }
+
+    public void setRoomNumber(String newRoomNumber){
+        roomNumber = newRoomNumber;
+    }
+
+    public void setFullName(String newFullName){
+        fullName = newFullName;
     }
 
 }
