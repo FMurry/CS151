98f5eab54bd4bb1ff027b81dc669410987692afc
Jason Safaiyeh <jsafaiyeh@gmail.com>
2015-11-15 19:18:20 -0800

    Template toolbar

---
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/courses/CoursesFragment.java
@@ -0,0 +1,13 @@
+package com.sixamigos.sjsucanvasapp.courses;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class CoursesFragment extends Fragment {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setC
+  }
+}
