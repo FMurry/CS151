827df4ccb1f16deb057ba38c4fa6cd105a57e9fc
Frederic <frederic.wesley.murry@gmail.com>
2015-11-15 19:23:44 -0800

    Course - Add professor
    (Course class is for custom user classes)

--- app/src/main/java/com/sixamigos/sjsucanvasapp/Courses/Course.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/Courses/Course.java
@@ -1,18 +1,20 @@
 package com.sixamigos.sjsucanvasapp.Courses;
 
+
 /**
  * Created by Frederic Murry on 11/12/15.
  * Course Object
  *
  */
-public class Course {
+public class Course  {
 
     private int id;
     private String courseName; //Ex: 151
     private String roomNumber; //Ex: DH 135
     private String fullName; //Ex: Object Oriented Programming
-    private double grade;
-
+    private double grade; // Grade of course Ex. 89.90
+    private String professor;
+    private String professorOffice;
 
     //private List<Assignments> assignments;
 
@@ -60,11 +62,26 @@ public class Course {
         return roomNumber;
     }
 
+    public String getFullName(){
+        return fullName;
+    }
     /**
      * Access the grade
      * @return
      */
     public double getGrade(){ return grade;}
+
+    /**
+     * Access the professor name
+     * @return
+     */
+    public String getProfessor() {return professor;}
+
+    /**
+     * Access the professor office
+     * @return
+     */
+    public String getProfessorOffice() {return professorOffice;}
 
 
     //Mutators_______________________________________________
@@ -73,8 +90,7 @@ public class Course {
      * Set the course name (different from full name) Ex CS 151
      * @param newCourseName
      */
-    public void setCourseName(String newCourseName){ courseName = newCourseName;
-    }
+    public void setCourseName(String newCourseName){ courseName = newCourseName;}
 
     /**
      * Sets the room number of course
@@ -98,6 +114,9 @@ public class Course {
      */
     public void setGrade(double newGrade) { grade = newGrade;}
 
+    public void setProfessor(String newProfessor) {professor = newProfessor;}
+
+    public void setProfessorOffice(String newProfessorOffice) {professorOffice = newProfessorOffice;}
 
     //Methods_________________________
     /**
@@ -107,6 +126,7 @@ public class Course {
     public String getLetterGrade(){
         throw  new UnsupportedOperationException("Letter Grade not yet supported");
     }
+
 
 
 
