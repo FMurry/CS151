170eea9e8bb37052c77bde1a51d75eadc81d117a
Alex Heritier <alex.heritier@gmail.com>
2015-11-18 16:50:19 -0800

    stuff

--- app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/GetCoursesTask.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/GetCoursesTask.java
@@ -24,49 +24,11 @@ public class GetCoursesTask extends AsyncTask<HashMap<String, String>, String, J
     @Override
     protected JSONObject doInBackground(HashMap<String, String>[] params) {
         JSONObject result = null;
-        /*try {
-            result = ServerTaskUtility.sendData(URL, params[0]);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.toString());
-            return null;
-        }*/
         try {
-            URL url = new URL(COURSE_URL);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-            Authenticator.setDefault(new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("Bearer", CanvasConnector.DEBUG_TOKEN.toCharArray());
-                }
-            });
-
-            System.out.println("Original URL: " + con.getURL());
-            System.out.println("response code: " + con.getResponseCode());
-            con.connect();
-            System.out.println("Connected URL: " + con.getURL());
-            System.out.println("response code: " + con.getResponseCode());
-            InputStream is = con.getInputStream();
-            System.out.println("Redirected URL: " + con.getURL());
-            System.out.println("response code: " + con.getResponseCode());
-            is.close();
-            /*
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            //BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            int responseCode = urlConnection.getResponseCode();
-            System.out.println(responseCode);
-            String line = "";
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
-            urlConnection.disconnect();
-            */
+            result = ServerTaskUtility.sendData(COURSE_URL, params[0]);
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.toString());
-            return null;
         }
         return result;
     }
--- app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/ServerTaskUtility.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/ServerTaskUtility.java
@@ -8,9 +8,11 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,7 +35,11 @@ public class ServerTaskUtility {
 
     public static JSONObject sendData(String url, HashMap<String, String> data) throws JSONException {
         // Create a new HttpClient and Post Header
+        /*
         HttpClient httpclient = new DefaultHttpClient();
+        HttpParams params = httpclient.getParams();
+        HttpConnectionParams.setConnectionTimeout(params, 3000);
+        HttpConnectionParams.setSoTimeout(params, 3000);
         HttpPost httppost = new HttpPost(url);
 
         try {
@@ -75,6 +81,56 @@ public class ServerTaskUtility {
             String jsonString = stringBuilder.toString();
             System.out.println(jsonString);
             return new JSONObject(jsonString.substring(9, jsonString.length())); // remove the while(1)
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpParams params = httpclient.getParams();
+        HttpConnectionParams.setConnectionTimeout(params, 3000);
+        HttpConnectionParams.setSoTimeout(params, 3000);
+
+        try {
+            // Add your data
+            String requestUrl = url;
+            Iterator iterator = data.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, String> entry = (Map.Entry) iterator.next();
+                BasicNameValuePair nameValuePair = new BasicNameValuePair(entry.getKey(), entry.getValue());
+                System.out.println(nameValuePair);
+                requestUrl += "?" + nameValuePair.getName() + "=" + nameValuePair.getValue();
+            }
+            HttpGet httpget = new HttpGet(url);
+
+            // Execute HTTP Post Request
+            for (Header h : httpget.getAllHeaders()) {
+                System.out.println(h.toString());
+            }
+            System.out.println(httpget.getURI());
+            HttpResponse response = httpclient.execute(httpget);
+
+            // According to the JAVA API, InputStream constructor do nothing.
+            //So we can't initialize InputStream although it is not an interface
+            InputStream inputStream = response.getEntity().getContent();
+
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String bufferedStrChunk = null;
+
+            while((bufferedStrChunk = bufferedReader.readLine()) != null){
+                stringBuilder.append(bufferedStrChunk);
+            }
+
+            //Log.d(TAG, stringBuilder.toString());
+            String jsonString = stringBuilder.toString();
+            System.out.println(jsonString);
+            return new JSONObject(jsonString.substring(0, jsonString.length())); // remove the while(1)
 
         } catch (ClientProtocolException e) {
             e.printStackTrace();
