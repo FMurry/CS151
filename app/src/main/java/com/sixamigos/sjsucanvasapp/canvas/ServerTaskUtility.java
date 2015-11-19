551b6d11c0097bcacd43bd0b6579cec5e44b9237
Alex Heritier <alex.heritier@gmail.com>
2015-11-18 16:50:19 -0800

    stuff

--- app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/ServerTaskUtility.java
+++ app/src/main/java/com/sixamigos/sjsucanvasapp/canvas/ServerTaskUtility.java
@@ -8,9 +8,11 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,22 +36,24 @@ public class ServerTaskUtility {
     public static JSONObject sendData(String url, HashMap<String, String> data) throws JSONException {
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost(url);
+        HttpParams params = httpclient.getParams();
+        HttpConnectionParams.setConnectionTimeout(params, 3000);
+        HttpConnectionParams.setSoTimeout(params, 3000);
 
         try {
             // Add your data
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            String requestUrl = url;
             Iterator iterator = data.entrySet().iterator();
             while (iterator.hasNext()) {
                 Map.Entry<String, String> entry = (Map.Entry) iterator.next();
                 BasicNameValuePair nameValuePair = new BasicNameValuePair(entry.getKey(), entry.getValue());
                 System.out.println(nameValuePair);
-                nameValuePairs.add(nameValuePair);
+                requestUrl += "?" + nameValuePair.getName() + "=" + nameValuePair.getValue();
             }
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpGet httpget = new HttpGet(url);
 
             // Execute HTTP Post Request
-            HttpResponse response = httpclient.execute(httppost);
+            HttpResponse response = httpclient.execute(httpget);
 
             // According to the JAVA API, InputStream constructor do nothing.
             //So we can't initialize InputStream although it is not an interface
@@ -70,7 +74,7 @@ public class ServerTaskUtility {
             //Log.d(TAG, stringBuilder.toString());
             String jsonString = stringBuilder.toString();
             System.out.println(jsonString);
-            return new JSONObject(jsonString.substring(9, jsonString.length())); // remove the while(1)
+            return new JSONObject(jsonString.substring(0, jsonString.length())); // remove the while(1)
 
         } catch (ClientProtocolException e) {
             e.printStackTrace();
